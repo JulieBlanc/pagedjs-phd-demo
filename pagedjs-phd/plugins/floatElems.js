d6254fec482ed00fc2c1f906a219f0e39b3de1ea
Julie Blanc <contact@julie-blanc.fr>
2024-12-15 16:32:37 +0100

    float next-page

--- pagedjs-phd/plugins/floatElems.js
+++ pagedjs-phd/plugins/floatElems.js
@@ -4,23 +4,19 @@ import { addcss } from './utils/addcss.js';
 export class floatElems extends Handler {
     constructor(chunker, polisher, caller) {
         super(chunker, polisher, caller);
-        this.selectorRight = new Set();
         this.selectorBottom = new Set();
         this.selectorTop = new Set();
-        this.selectorMoveElem  = new Set();
-        this.selectorColInside2 = new Set();
-
+        this.selectorNextPage = new Set();
+        this.nextPageElem = new Set();
 
     }
 
 
-    beforeParsed(content){  
-        console.log("floatElems --------- ")
-    }
+   
 
     onDeclaration(declaration, dItem, dList, rule) {
         // Read customs properties
-        if (declaration.property == "--pagedjs-float-img") {
+        if (declaration.property == "--pagedjs-float-elem") {
           // get selector of the declaration (NOTE: need csstree.js)
           let selector = csstree.generate(rule.ruleNode.prelude);
           // Push selector in correct set 
@@ -28,6 +24,8 @@ export class floatElems extends Handler {
             this.selectorBottom.add(selector);
           }else if(declaration.value.value.includes("top")) {
             this.selectorTop.add(selector);
+          }else if(declaration.value.value.includes("next-page")) {
+            this.selectorNextPage.add(selector);
           }
         }
      
@@ -39,7 +37,7 @@ export class floatElems extends Handler {
         for (let item of this.selectorBottom) {
             let elems = parsed.querySelectorAll(item);
             for (let elem of elems) {
-                elem.classList.add("float-img_bottom");
+                elem.classList.add("float-elem_bottom");
             }
         }
 
@@ -47,31 +45,56 @@ export class floatElems extends Handler {
         for (let item of this.selectorTop) {
             let elems = parsed.querySelectorAll(item);
             for (let elem of elems) {
-                elem.classList.add("float-img_top");
+                elem.classList.add("float-elem_top");
             }
         }
+
+        // next-page
+        for (let item of this.selectorNextPage) {
+          let elems = parsed.querySelectorAll(item);
+          for (let elem of elems) {
+              elem.classList.add("float-elem_next-page");
+              elem.style.position = "absolute";
+          }
+      }
     } 
 
 
-    afterPageLayout(pageElement, page, breakToken){
-		let floatTopPage = pageElement.querySelector(".float-img_top");
+  afterPageLayout(pageElement, page, breakToken){
+		let floatTopPage = pageElement.querySelector(".float-elem_top");
     let pageContent = pageElement.querySelector(".pagedjs_page_content");
     if(floatTopPage){
       pageContent.insertBefore(floatTopPage, pageContent.firstChild);
     }
 
-    let floatBottomPage = pageElement.querySelector(".float-img_bottom");
+    let floatBottomPage = pageElement.querySelector(".float-elem_bottom");
     if(floatBottomPage){
       pageContent.insertBefore(floatBottomPage, pageContent.firstChild);
       floatBottomPage.style.position = "absolute";
       floatBottomPage.style.bottom = "0px";
     }
 
+
+    let nextPageElems = pageElement.querySelectorAll(".float-elem_next-page:not([data-processed])");
+    nextPageElems.forEach((elem) => {
+      this.nextPageElem.add(elem); 
+      elem.setAttribute("data-processed", "true"); 
+      elem.remove();
+    });
 	}
 
- 
 
-
+  onOverflow(overflow, rendered, bounds) {
+    for (let elem of this.nextPageElem) {
+      if (!elem.hasAttribute("data-handled")) {
+        const copy = elem.cloneNode(true);
+        copy.style.position = "relative";
+        rendered.insertBefore(copy, rendered.firstChild);
+        elem.setAttribute("data-handled", "true"); // Marque comme traité
+      }
+    }
+    this.nextPageElem.clear();
+  }
 
 
   
@@ -79,5 +102,3 @@ export class floatElems extends Handler {
 
 
 
-
-    
